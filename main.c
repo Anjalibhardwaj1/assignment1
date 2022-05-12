d7285787ad56f631aa628205d26ce2148d502903
mobeenulhaq <raomobeen@hotmail.co.uk>
2022-05-12 13:52:32 -0400

    changes to .c and .h files

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "Aggregate.h"
+#include "Aggregate.c"
+
+int main() {
+
+    float (*aggregates[5])(float*, int) = {&min, &max, &sum, &avg, &pseudo_avg};
+
+    float arr1[5] = {4.6, 2.2, 8.7, 4.3, 11.5};
+    float arr2[10] = {0.2, 5.3, 8.9, 1.6, 8.2, 5.0, 1.2, 4.6, 2.3, 9.4};
+
+    for (int i = 0; i < 5; i++) {
+
+        printf("%f\n", (*(aggregates[i]))(arr1, 5));
+        printf("%f\n", (*(aggregates[i]))(arr2, 10));
+
+    }
+
+    return 0;
+
+}
