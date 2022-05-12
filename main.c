bca5b6b9cccce5a77674cc35edac2ce38d921076
mobeenulhaq <raomobeen@hotmail.co.uk>
2022-05-12 17:57:17 -0400

    error handling (aggregate.c)

--- main.c
+++ main.c
@@ -4,21 +4,17 @@
 
 int main() {
 
-    //float (*aggregates[5])(float*, int) = {&min, &max, &sum, &avg, &pseudo_avg};
+    float (*aggregates[5])(float*, int) = {&min, &max, &sum, &avg, &pseudo_avg};
 
     float arr1[5] = {4.6, 2.2, 8.7, 4.3, 11.5};
+    float arr2[10] = {0.2, 5.3, 8.9, 1.6, 8.2, 5.0, 1.2, 4.6, 2.3, 9.4};
 
-    printf("%f\n", pseudo_avg(arr1, -1));
+    for (int i = 0; i < 5; i++) {
 
-    //float arr1[5] = {4.6, 2.2, 8.7, 4.3, 11.5};
-    //float arr2[10] = {0.2, 5.3, 8.9, 1.6, 8.2, 5.0, 1.2, 4.6, 2.3, 9.4};
+        printf("%f\n", (*(aggregates[i]))(arr1, 5));
+        printf("%f\n", (*(aggregates[i]))(arr2, 10));
 
-    //for (int i = 0; i < 5; i++) {
-
-        //printf("%f\n", (*(aggregates[i]))(arr1, 5));
-        //printf("%f\n", (*(aggregates[i]))(arr2, 10));
-
-    //}
+    }
 
     return 0;
 
