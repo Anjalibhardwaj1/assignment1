d00bf9d98be0c467506eb01c747be41ee04c56a7
mobeenulhaq <raomobeen@hotmail.co.uk>
2022-05-12 19:22:49 -0400

    removing unnecessary comments

--- Aggregate.c
+++ Aggregate.c
@@ -78,19 +78,3 @@ float pseudo_avg(float *a, int size){
     }
 
 }
-
-//int main()
-//{
-
-    //float arr[] = {1, 4, 5, 6.8, -1};
-    //float average = avg(arr, 5);
-    //printf("%f\n", average);
-
-    //q3
-    //for inputs that are null or no positive give a "fatal error" and the program is aborted. 
-
-    //q4
-    //define two arrays of float numbers that contain 5 and 10 respectively. 
-    //float arr1[5] = {4.6, 2.2, 8.7, 4.3};
-    //float arr2[10] = {2.4, 5.3, 8.9, 1.6, 8.2, 5.0, 1.2, 4.6, 2.3, 9.4};
-//}
--- Aggregate.h
+++ Aggregate.h
@@ -11,14 +11,3 @@ float sum(float *a, int size);
 float avg(float *a, int size);
 
 float pseudo_avg(float *a, int size);
-
-
-//q4
-//define an array of pointer functions called aggregates
-//contain 5 pointers to the 5 functions
-//float (*aggregates[5]) (float) = {&min, &max, &sum, &avg, &pseudo_avg};
-
-
-//write a short for loop that iterates through an array of functions and calls each function on the two sample arrays you defined. the loop prints the calculated values in 10 
-//separate lines. 
-
