60ccf4f1d79d20f1d513c4ee4af5d5583345b660
mobeenulhaq <raomobeen@hotmail.co.uk>
2022-05-12 17:54:24 -0400

    error handling in aggregate.c

--- Aggregate.c
+++ Aggregate.c
@@ -2,41 +2,81 @@
 #include "Aggregate.h"
 
 float min(float *a, int size){
-    float min = a[0];
-    for(int i = 0; i < size; i++){
-        if (a[i] < min){
-            min = a[i];
+
+    if (size >= 0) {
+
+        float min = a[0];
+        for(int i = 0; i < size; i++){
+            if (a[i] < min){
+                min = a[i];
+            }
         }
+        return min;
+
+    } else {
+        printf("FATAL ERROR at line %d\n", ((__LINE__)-9));
     }
-    return min;
 }
 
 float max(float *a, int size){
-    float max = a[0];
-    for(int i = 0; i < size; i++){
-        if (a[i] > max){
-            max = a[i];
+
+    if (size >= 0) {
+
+        float max = a[0];
+        for(int i = 0; i < size; i++){
+            if (a[i] > max){
+                max = a[i];
+            }
         }
+        return max;
+
+    } else {
+        printf("FATAL ERROR at line %d\n", ((__LINE__)-9));
     }
-    return max;
+
+    
 }
 
 float sum(float *a, int size){
-    float sum;
-    for(int i = 0; i < size; i++){
-        sum = sum + a[i];
+
+    if (size >= 0) {
+
+        float sum;
+        for(int i = 0; i < size; i++){
+            sum = sum + a[i];
+        }
+        return sum;
+
+    } else {
+        printf("FATAL ERROR at line %d\n", ((__LINE__)-5));
     }
-    return sum;
+
 }
 
 float avg(float *a, int size){
-    float avg = sum(a, size)/size;
-    return avg;
+
+    if (size >= 0) {
+
+        float avg = sum(a, size)/size;
+        return avg;
+
+    } else {
+        printf("FATAL ERROR at line %d\n", ((__LINE__)-4));
+    }
+
 }
 
 float pseudo_avg(float *a, int size){
-    float value = (min(a, size)+max(a, size))/2;
-    return value;
+
+    if (size >= 0) {
+
+        float value = (min(a, size)+max(a, size))/2;
+        return value;
+
+    } else {
+        printf("FATAL ERROR at line %d\n", ((__LINE__)-4));
+    }
+
 }
 
 //int main()
